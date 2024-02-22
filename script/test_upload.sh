e8b8239398cb1a27b968a8dadaa6c0329b5ba828
Eric Ding <ericdingg16@outlook.com>
2024-02-22 18:37:57 -0500

    Impl test upload download script

---
+++ script/test_upload.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+total_time=0
+num_runs=3
+
+for ((i=1; i<=$num_runs; i++))
+do
+    result=$(curl -o /dev/null -s -w "Total time: %{time_total}s\n" -T "object/$1" "http://localhost:8787/$2" -X PUT)
+    
+    # Extracting the time from the result using awk
+    time=$(echo $result | awk '{print $3}' | sed 's/s//')
+    
+    # Adding the time to the total
+    total_time=$(echo "$total_time + $time" | bc)
+done
+
+# Calculating the average time
+average_time=$(echo "scale=5; $total_time / $num_runs" | bc)
+
+echo "Average Time: $average_time seconds"
