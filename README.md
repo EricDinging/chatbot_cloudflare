77bd995ddffac474c4c8740036a1581811610ce0
Eric Ding <ericdingg16@outlook.com>
2024-02-22 17:03:20 -0500

    Impl file upload and download logic

--- README.md
+++ README.md
@@ -3,6 +3,7 @@ A simple GPT-powered cloudflare-powered web search engine in Javascript.
 Enter a question, and the server will call GPT for a website url that is related to this question. 
 Useful contents from target webpages will then be shown here!
 
+File analysis functionality is under construction. 
 ## Configure npm environment
 ```
 npm i create-cloudflare
@@ -21,6 +22,13 @@ Deploy cloudflare workers.
 ```
 npx wrangler deploy
 ```
+## Object store testing
+```
+# Upload a file to local dev server
+curl -T object/dummyfile.txt http://localhost:8787/dummy -X PUT 
+# Download
+curl --output result.txt http://localhost:8787/dummy -X GET 
+```      
 ## Reference
 https://developers.cloudflare.com/workers/tutorials/openai-function-calls-workers/
 https://platform.openai.com/docs/guides/function-calling?lang=node.js
--- src/index.js
+++ src/index.js
@@ -76,28 +76,53 @@ async function call_gpt_get_website(query, key) {
 
 export default {
 	async fetch(request, env, ctx) {
-		if (request.method === "GET") {
-			return new Response(template(request.cf), {
-				headers: {
-					"content-type": "text/html;charset=UTF-8",
-				},
-			});
-		} else if (request.method === "POST") {
-			const formData = await request.formData();
-			const question = formData.get('question');
-			const key = env.OPENAI_API_KEY;
-			const response = await call_gpt_get_website(question, key);
-			return new Response(response, {
-				headers: {
-					"content-Type": "text/plain",
-				},
-			});
-		} else {
-			return new Response('Method Not Allowed', {
-				status: 405,
-				statusText: 'Method Not Allowed',
-			})
+		const url = new URL(request.url);
+		const object_name = url.pathname.slice(1);
+		switch (request.method) {
+			case "GET":
+				if (object_name) {
+					// Retrieve from R2 data store
+					console.log(object_name);
+					const object = await env.MY_BUCKET.get(object_name);
+					if (object === null) {
+						return new Response('Object not found', { status: 404 });
+					}
+					const headers = new Headers();
+					object.writeHttpMetadata(headers);
+					headers.set('etag', object.httpEtag);
+					console.log(object.body)
+					return new Response(object.body, {headers,});
+				} else {
+					// Default web page
+					return new Response(template(request.cf), {
+						headers: {
+							"content-type": "text/html;charset=UTF-8",
+						},
+					});
+				}
+			case "PUT":
+				await env.MY_BUCKET.put(object_name, request.body);
+        		return new Response(`Put ${object_name} successfully!`);
+			
+			case 'DELETE':
+				await env.MY_BUCKET.delete(object_name);
+				return new Response('Deleted!');
+
+			case "POST":
+				const formData = await request.formData();
+				const question = formData.get('question');
+				const key = env.OPENAI_API_KEY;
+				const response = await call_gpt_get_website(question, key);
+				return new Response(response, {
+					headers: {
+						"content-Type": "text/plain",
+					},
+				});
+			default:
+				return new Response('Method Not Allowed', {
+					status: 405,
+					statusText: 'Method Not Allowed',
+				})
 		}
-		
 	},
 }
